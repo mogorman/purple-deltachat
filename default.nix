186c65292ba1277f5498d528edf78d4da2ba7457
Matthew O'Gorman <mog@rldn.net>
2018-03-11 21:58:24 -0400

    updated trying to use meson with the null client

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 with (import <nixpkgs> {});
 stdenv.mkDerivation {
   name = "delta-chat-core";
-  buildInputs = [ deltachat-core pkgconfig pidgin ];
+  buildInputs = [ deltachat-core pkgconfig pidgin meson ];
   shellHook = ''
     export PS1="purple-deltachat> "
 '';
